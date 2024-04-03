db17a7f29bf23758d9581431b27b748fecd22dab
Kiran Ajith <kiranajith97@gmail.com>
2024-04-03 19:23:15 -0400

    updated simple publisher

--- project3_pkg/project3_pkg/project3.py
+++ project3_pkg/project3_pkg/project3.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 
-class PathPlannerNode(Node):
+def timer_callback():
+    global velocity_publisher
+    velocity_msg = Twist()
+    velocity_msg.linear.x = 1.0
+    velocity_publisher.publish(velocity_msg)
 
-    def __init__(self):
-        super().__init__('path_planner')
-        self.velocity_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
-        self.timer = self.create_timer(0.5,self.timer_callback)
-
-
-    def timer_callback(self):
-        velocity_msg = Twist()
-        velocity_msg.linear.x = 1.0
-        self.velocity_publisher.publish(velocity_msg)
-
-
-
-
-def main(args=None):
-    rclpy.init(args=args)
-    node = PathPlannerNode()
+def main():
+    rclpy.init(args=None)
+    node = Node('path_planner')
+    global velocity_publisher
+    velocity_publisher = node.create_publisher(Twist, 'cmd_vel', 10)
+    timer = node.create_timer(0.5, timer_callback)
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
